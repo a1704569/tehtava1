6d2dbc46f439a45c0b37e0e57723622f6c4ba713
Otto Ruokonen <a1704569@myy.haaga-helia.fi>
2021-01-21 15:16:05 +0200

    korjauksia

--- tehtava1/src/main/java/com/example/tehtava1/web/HelloController.java
+++ tehtava1/src/main/java/com/example/tehtava1/web/HelloController.java
@@ -1,5 +1,22 @@
 package com.example.tehtava1.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class HelloController {
+	@RequestMapping ("/hello")
+	public String hello
+		(@RequestParam (value= "name") String name
+		, @RequestParam (value= "location") String location
+		, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("location", name);
+		return "Welcome to the " +location+ " " + name ;
+	}
 
 }
--- tehtava1/src/main/java/com/example/tehtava1/web/IndexController.java
+++ tehtava1/src/main/java/com/example/tehtava1/web/IndexController.java
@@ -1,5 +1,18 @@
 package com.example.tehtava1.web;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-}
+@Controller
+@ResponseBody
+public class IndexController { 
+		@RequestMapping ("/index")
+		public String index() {
+		return "This is the Main page";
+		}
+	}	
+
+		
+
+
